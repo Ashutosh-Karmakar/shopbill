9aa547364625bcb2db7c8555dbf5d54bb1799be5
Ashutosh-Karmakar <60296760+Ashutosh-Karmakar@users.noreply.github.com>
2021-12-08 20:02:39 +0530

    Add files via upload

--- third.py
+++ third.py
@@ -24,8 +24,6 @@ window.attributes('-fullscreen', True)
 window.configure(bg='#FFE6BC')
 
 window.title("Giridhari Jewellery")
-def enter(event):
-    print('Button-2 pressed at x = % d, y = % d'%(event.x, event.y))
 
 def exit_(event):
     if(tkinter.messagebox.askokcancel('QUIT','Do You Want to Quit??')):
@@ -35,13 +33,11 @@ def exit_(event):
 
  
 
-window.bind('<Return>', enter)
 window.bind('<Escape>', exit_)
 
 bg_color = "#FFE6BC"
 
-# creating text label to display on window screen
-# ==================================================================================
+# ==================================creating the gui================================================
 
 Name=Label(window, text='Name and Address:', font=('times new rommon',10),bg=bg_color)
 Name.grid(row=1,column=0,padx=10,pady=5)
@@ -76,57 +72,62 @@ siLabel = Label(F2,text="Sino.",font=('times new rommon',10),bg=bg_color)
 siLabel.grid(column=0,row=0)
 si_txt = []
 for i in range(1,10):
-    txt=Entry(F2,width=3,font='arial 12',bd=1,justify=CENTER)
+    txt=Label(F2,text=i,font=('times new rommon',10),bg=bg_color)
     txt.grid(row=i,column=0,padx=0,pady=5)
     si_txt.append(txt)
+
 
 desLabel = Label(F2,text="Description",font=('times new rommon',10),bg=bg_color)
 desLabel.grid(column=1,row=0)
 des_txt = []
-for i in range(1,10):
-    txt=Entry(F2,width=80,font='arial 12',bd=1,justify=CENTER)
-    txt.grid(row=i,column=1,padx=5,pady=5)
-    des_txt.append(txt)
 
 wtLabel = Label(F2,text="Weight",font=('times new rommon',10),bg=bg_color)
 wtLabel.grid(column=2,row=0)
 wt_txt = []
-for i in range(1,10):
-    txt=Entry(F2,width=9,font='arial 12',bd=1,justify=CENTER)
-    txt.grid(row=i,column=2,padx=5,pady=5)
-    wt_txt.append(txt)
 
 unitLabel = Label(F2,text="Unit Price",font=('times new rommon',10),bg=bg_color)
 unitLabel.grid(column=3,row=0)
 unitLabel_txt = []
-for i in range(1,10):
-    txt=Entry(F2,width=9,font='arial 15',bd=1,justify=CENTER)
-    txt.grid(row=i,column=3,padx=5,pady=5)
-    unitLabel_txt.append(txt)
 
 cgstLabel = Label(F2,text="CGST(1.5%)",font=('times new rommon',10),bg=bg_color)
 cgstLabel.grid(column=4,row=0)
 cgstLabel_txt = []
-for i in range(1,10):
-    txt=Entry(F2,width=10,font='arial 12',bd=1,justify=CENTER)
-    txt.grid(row=i,column=4,padx=5,pady=5)
-    cgstLabel_txt.append(txt)
 
 sgstLabel = Label(F2,text="SGST(1.5%)",font=('times new rommon',10),bg=bg_color)
 sgstLabel.grid(column=5,row=0)
 sgstLabel_txt = []
-for i in range(1,10):
-    txt=Entry(F2,width=10,font='arial 12',bd=1,justify=CENTER)
-    txt.grid(row=i,column=5,padx=5,pady=5)
-    sgstLabel_txt.append(txt)
 
 toLabel = Label(F2,text="Net",font=('times new rommon',10),bg=bg_color)
 toLabel.grid(column=6,row=0)
 toLabel_txt = []
+
 for i in range(1,10):
-    txt=Entry(F2,width=16,font='arial 12',bd=1,justify=CENTER)
-    txt.grid(row=i,column=6,padx=5,pady=5)
-    toLabel_txt.append(txt)
+    txt1=Entry(F2,width=80,font='arial 12',bd=1,justify=CENTER)
+    txt1.grid(row=i,column=1,padx=5,pady=5)
+    des_txt.append(txt1)
+
+    txt2=Entry(F2,width=9,font='arial 12',bd=1,justify=CENTER)
+    txt2.grid(row=i,column=2,padx=5,pady=5)
+    wt_txt.append(txt2)
+
+    txt3=Entry(F2,width=9,font='arial 15',bd=1,justify=CENTER)
+    txt3.grid(row=i,column=3,padx=5,pady=5)
+    unitLabel_txt.append(txt3)
+
+    txt4=Entry(F2,width=10,font='arial 12',bd=1,justify=CENTER)
+    txt4.grid(row=i,column=4,padx=5,pady=5)
+    cgstLabel_txt.append(txt4)
+
+    txt5=Entry(F2,width=10,font='arial 12',bd=1,justify=CENTER)
+    txt5.grid(row=i,column=5,padx=5,pady=5)
+    sgstLabel_txt.append(txt5)
+
+    txt6=Entry(F2,width=16,font='arial 12',bd=1,justify=CENTER)
+    txt6.grid(row=i,column=6,padx=5,pady=5)
+    toLabel_txt.append(txt6)
+
+
+
 
 # ======================================================================================================================
 
@@ -164,29 +165,146 @@ netotal_l.grid(column=3,row=0)
 netotal= Entry(F4,width=15,font='arial 14',bd=1,justify=CENTER)
 netotal.grid(row=0,column=4,padx=10,pady=5)
 
+
+
+# ==========================================enter key binding===========================================
+
+Name_txt.focus()
+
+# define a function to change the tab order
+# def tab_order(event):
+#     widget = [Name_txt,addhar_txt,mobile_txt,bill_txt]
+#     for w in widget:
+#         w.lift()
+
+# def enter(event):
+#     print('Button-2 pressed at x = % d, y = % d'%(event.x, event.y))
+
+# window.bind('<Return>', tab_order)
+
+
+
+
+
+# ======================================function of the Code================================================
+def generateBill():
+    wb = openpyxl.Workbook()
+    sh1 = wb.active
+    sh1['E1'] = "GIRIDHARI JEWELLERY"
+    sh1['E1'].font = Font(name='times new rommon',size=25,bold=False,italic=False,vertAlign=None,underline='none',strike=False,color='C85C5C')
+
+    sh1['D2'] = "Badheibanka Chawk, Old Town, Bhubaneswar, Mob.: 9090280083,9861230757"
+    sh1['D2'].font = Font(name='times new rommon',size=12,bold=False,italic=False,vertAlign=None,underline='none',strike=False,color='064635')
+
+    sh1['F3'] = "916 GOLD AND SILVER ORNAMENT"
+    sh1['F3'].font = Font(name='times new rommon',size=10,bold=False,italic=False,vertAlign=None,underline='none',strike=False,color='C85C5C')
+
+    # sh1['A3'] = "Name"
+    thin_border = Border(left=Side(border_style='dashed',color='FF000000'),
+                right=Side(border_style='dashed',color='FF000000'),
+                top=Side(border_style='thin',color='FF000000'),
+                bottom=Side(border_style='thin',color='FF000000')
+                )
+                
+    thick_border_left = Border(left=Side(border_style='medium',color='FF000000'))
+    thick_border_right = Border(right=Side(border_style='medium',color='FF000000'))
+    thick_border_top = Border(top=Side(border_style='medium',color='FF000000'))
+    thick_border_bottom = Border(bottom=Side(border_style='medium',color='FF000000'))
+                    
+    Double_border = Border(left=Side(border_style='dashed',color='FF000000'),
+                    right=Side(border_style='dashed',color='FF000000'),
+                    top=Side(border_style='double',color='FF000000'),
+                    bottom=Side(border_style='double',color='FF000000')
+                    )
+    fill_cell = PatternFill(fill_type=fills.FILL_SOLID,start_color='00FFFF00',end_color='00FFFF00')
+    #define size of the table 
+    row_num=34
+    col_num=14
+    #location of the Table 
+    row_loc=4
+    col_loc=0
+    dis=2 # distance between the tables 
+    for i in range (row_loc,row_loc+row_num):
+        for j in range (col_num+col_loc):
+            
+            if(i==row_loc and j!=col_loc+col_num-1 and j!=col_loc):
+                sh1.cell(row=i+1, column=j+1).border=thick_border_top
+            if(i==row_loc+row_num-1 and j!=col_loc+col_num-1 and j!=col_loc):
+                    sh1.cell(row=i+1, column=j+1).border=thick_border_bottom
+            
+            if(i!=row_loc+row_num-1 and j==col_loc+col_num-1 and i!=row_loc):
+                sh1.cell(row=i+1, column=j+1).border=thick_border_right
+            if(i!=row_loc and j==col_loc and i!=row_loc+row_num-1):
+                sh1.cell(row=i+1, column=j+1).border=thick_border_left
+
+    # sh1.cell(row=row_loc,column=col_loc).border=thick_border_top
+    # sh1.cell(row=row_loc,column=col_loc).border=thick_border_left
+    # sh1.cell(row=row_loc+row_num-1,column=col_loc).border=thick_border_bottom
+
+    sh1.cell(row=row_loc+row_num,column=col_loc+col_num).border=Border(bottom=Side(border_style='medium',color='FF000000'),
+    right=Side(border_style='medium',color='FF000000'))
+
+    sh1.cell(row=row_loc+1,column=col_loc+1).border=Border(left=Side(border_style='medium',color='FF000000'),
+    top=Side(border_style='medium',color='FF000000'))
+
+    sh1.cell(row=row_loc+row_num,column=col_loc+1).border=Border(left=Side(border_style='medium',color='FF000000'),
+    bottom=Side(border_style='medium',color='FF000000'))
+
+    sh1.cell(row=row_loc+1,column=col_loc+col_num).border=Border(right=Side(border_style='medium',color='FF000000'),
+    top=Side(border_style='medium',color='FF000000'))
+    
+    
+    # sh1.cell(row=row_loc,column=col_loc+col_num-1).border=thick_border_right
+
+
+            # if i==row_loc:
+            #     sh1.cell(row=i+1, column=j+2).border=Double_border
+            #     sh1.cell(row=i+1, column=j+2).fill=fill_cell
+            # if i==row_loc+row_num-1:
+            #     sh1.cell(row=i+1, column=j+2).border=thick_border
+    row_loc= row_loc+row_num+dis 
+
+    
+
+
+    openpyxl.worksheet.worksheet.Worksheet.set_printer_settings(sh1, paper_size = 1, orientation='landscape')
+    sh1.page_margins.top=.2
+    sh1.page_margins.right=.2
+    sh1.page_margins.bottom=.2
+
+
+
+    wb.save(filename='test.xlsx')
+
+
+def prin(event):
+    os.startfile('test.xlsx','print')
+
+def opena():
+    print("Hello")
+    os.system('test.xlsx')
 # =================================================================================================================
 
 F5 = LabelFrame(window,bg= "#519259")
 F5.place(x=5,y=570,width=600,height=120)
 
-newBtn = Button(F5,text="New",font=('times new rommon',13),bg=bg_color,bd=2)
+newBtn = Button(F5,text="New (Ctrl+N)",font=('times new rommon',13),bg=bg_color,bd=2)
 newBtn.grid(column=0,row=0,padx=20,pady=10)
 
-printBtn = Button(F5,text="Print",font=('times new rommon',13),bg=bg_color,bd=2)
+printBtn = Button(F5,text="Print (Ctrl+P)",font=('times new rommon',13),command=prin,bg=bg_color,bd=2)
 printBtn.grid(column=0,row=1,padx=20,pady=10)
 
-generateBtn = Button(F5,text="Generate Bill",font=('times new rommon',13),bg=bg_color,bd=2)
+generateBtn = Button(F5,text="Generate Bill (Ctrl+G)",font=('times new rommon',13),command=generateBill,bg=bg_color,bd=2)
 generateBtn.grid(column=1,row=0,padx=20,pady=10)
 
-findBtn = Button(F5,text = "Find",font=('times new rommon',13),bg=bg_color,bd=2)
+findBtn = Button(F5,text = "Find (Ctrl+F)",font=('times new rommon',13),command=open,bg=bg_color,bd=2)
 findBtn.grid(column=1,row=1,padx=20,pady=10)
 
 
 
+window.bind('<Control-G>', generateBill)
+window.bind('<Control-p>', prin)
+window.bind('<Control-slash>', opena)
 
-
-
-
-
-# =================================================================================================================
+# ========================================end of the code=========================================================================
 window.mainloop()
